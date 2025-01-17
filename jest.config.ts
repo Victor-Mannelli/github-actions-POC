8dedb6a71ea50c4f8f0cae875a429faaa5f7f0d9
mannelli <imp.mannelli@gmail.com>
2025-01-16 22:16:30 -0300

    feat: add moc function for testing and tests

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,15 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 
 const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest', // Transform TypeScript files
+  },
+  
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
